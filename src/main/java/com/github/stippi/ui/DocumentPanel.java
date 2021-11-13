9bc8643601612ff3b64de8c69943831161e911ac
Stephan Aßmus <superstippi@gmx.de>
2021-11-13 23:05:59 +0100

    Small refactorings

--- src/main/java/com/github/stippi/ui/DocumentPanel.java
+++ src/main/java/com/github/stippi/ui/DocumentPanel.java
@@ -1,10 +1,13 @@
 package com.github.stippi.ui;
 
 import com.github.stippi.model.Document;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
@@ -21,9 +24,11 @@ public class DocumentPanel extends Composite implements Panel {
         fDocument = document;
         fFields = new HashMap<>();
 
+        addTitle();
+
         String[] fields = {"Title", "Logline", "Summary"};
         for (String f : fields) {
-            setupTextField(f);
+            addTextField(f);
         }
 
         FocusManager.registerPanel("document", this);
@@ -44,7 +49,7 @@ public class DocumentPanel extends Composite implements Panel {
         }
     }
 
-    private void setupTextField(final String field) {
+    private void addTextField(final String field) {
         Label label = new Label(this, SWT.NULL);
         label.setText(field);
 
@@ -52,5 +57,23 @@ public class DocumentPanel extends Composite implements Panel {
         text.addModifyListener(modifyEvent -> fDocument.setValue(field, text.getText()));
         text.setLayoutData(new GridData(GridData.FILL, GridData.FILL, true, false));
         fFields.put(field, text);
+    }
+
+    private void addTitle() {
+        Label label = new Label(this, SWT.NULL);
+        label.setText("SWT native widgets");
+        setTitleFont(label);
+
+        GridData layoutData = new GridData(GridData.CENTER, GridData.CENTER, true, false);
+        layoutData.horizontalSpan = 2;
+        label.setLayoutData(layoutData);
+    }
+
+    private static void setTitleFont(Control control) {
+        FontData fontData = control.getFont().getFontData()[0];
+        fontData.setHeight(24);
+        Font titleFont = new Font(control.getDisplay(), fontData);
+        control.setFont(titleFont);
+        control.addDisposeListener(disposeEvent -> titleFont.dispose());
     }
 }
